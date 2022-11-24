6ac78ea60b00c32a6642a1e70a5a40f9c5f93418
Dirac Murairi <dmurairimukongya@gmail.com>
2022-11-25 00:09:06 +0200

    complete navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './components';
-import { Header, Footer } from './containers';
+import { Header } from './containers';
 
 const App = () => {
   return (
