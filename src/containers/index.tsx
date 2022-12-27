b1e7b49fb2a515d4cbf2d4ee91753f510ee0f518
Dirac Murairi <dmurairimukongya@gmail.com>
2022-12-27 23:15:14 +0200

    add possibility  section

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -3,3 +3,4 @@ export { default as Features } from "./features/features.container";
 export { default as Footer } from "./footer/footer.container";
 export { default as Header } from "./header/header.container";
 export { default as WhatGPT3 } from "./whatGPT3/whatGPT3.container";
+export { default as Possibility } from "./possibility/possibility.container";
